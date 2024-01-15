2693a1f505b0ecdaa90e7f28ffeb82e646055dd7
maironald <ronald.mai.goldenowl@gmail.com>
2024-01-15 11:03:19 +0700

    format: scss file

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -3,6 +3,6 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   connect() {
     this.element.textContent = 'Hello World!';
-    console.log("Hello: ", this.element)
+    console.log('Hello: ', this.element);
   }
 }
